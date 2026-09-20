fc89465bffa7aff76e2bc5a0412bf2833ab9f744
senay girma <senaygirma33@gmail.com>
2026-09-20 07:35:33 -0400

    build a follower counter

---
+++ followerscount/followers_count.js
@@ -0,0 +1,18 @@
+let count=0;
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML=count;
+}
+
+function checkCountValue(){
+    if(count===10){
+        alert("your instagram account gained 10 followers! congratulations!");
+    } else if(count===20){
+        alert("your instagram account gained 20 followers! keep it up!");
+    }
+}
