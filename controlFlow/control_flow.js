0552040da432e7a4274aa4fc9e010cb0aa6d92a0
senay <senaygirma33@gmail.com>
2026-09-23 06:25:56 -0400

    practiced if else, nested statements, and switch

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,47 @@
+let userRole= "admin";
+let accessLevel;
+
+if(userRole==="admin"){
+    accessLevel = "Full access granted";
+
+}else if(userRole==="manager"){
+    accessLevel="Limited access granted";
+} else{
+    accessLevel = "No access granted";
+}
+console.log("Access Level:", accessLevel);
+
+
+let isLoggedIn= true;
+let userMessage;
+
+if(isLoggedIn){
+    if(userRole==="admin"){
+        userMessage="Welcome, admin!";
+    }else{
+        userMessage="welcome, User!";
+    }
+}else{
+    userMessage="please log in to access the system.";
+}
+console.log("User Message:", userMessage);
+
+
+let userType= "subscriber";
+let userCatergory;
+
+switch(userType){
+    case "admin":
+        userCatergory = "Administrator";
+        break;
+    case "manager":
+        userCatergory = "Manager";
+        break;
+    case "subscriber":
+        userCatergory = "Subscriber"
+        break;
+    default:
+        userCatergory = "Unkown"
+}
+
+console.log("User category:", userCatergory);
